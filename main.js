3501453906091cc3de12df9458efab66fb5ad7ad
Devin Cooley <dcooley1350@gmail.com>
2020-01-24 09:39:48 -0800

    Write test data

--- main.js
+++ main.js
@@ -3,9 +3,11 @@ import "babel-polyfill"
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore, applyMiddleware } from 'redux'
-
+import createSagaMiddleware from 'redux-saga';
 import Counter from './Counter'
 import reducer from './reducers'
+import  { helloSaga } from './sagas'
+
 
 const store = createStore(reducer)
 
